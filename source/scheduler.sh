43663db123a6231edbc6e65f0e6294dbc3401d6b
Viktor Nikolaev <victor.n@indicaonline.com>
2020-11-19 14:48:26 +0300

    ~ source

--- source/scheduler.sh
+++ source/scheduler.sh
@@ -38,7 +38,22 @@ function check_time {
 }
 
 function worker {
- echo $1
+ local resource_id_type=$(echo $1 | jq -r '.resource_id_type[]' |tr -d '\n'  )
+ local resource_id=$(echo $1 | jq -r '.resource_id[]' |tr -d '\n'  )
+ local resource_region=$(echo $1 | jq -r '.resource_region[]' |tr -d '\n'  )
+ local operational=$(echo $1 | jq -r '.operational[]' |tr -d '\n'  )
+ local id=$(echo $1 | jq -r '.id[]' |tr -d '\n'  )
+ local scheduler_type=$(echo $1 | jq -r '.scheduler_type[]' |tr -d '\n'  )
+ local work_hours=$(echo $1 | jq -r '.work_hours[]' |tr -d '\n'  )
+ local resource_type=$(echo $1 | jq -r '.resource_type[]' |tr -d '\n'  )
+ echo "resource_id_type $resource_id_type"
+ echo "resource_id $resource_id"
+ echo "resource_region $resource_region"
+ echo "operational $operational"
+ echo "id $id"
+ echo "scheduler_type $scheduler_type"
+ echo "work_hours $work_hours"
+ echo "resource_type $resource_type"
 }
 
 function create_aws_profile {
