c2002e49bd118e31a95e0ba6114ff7e0bb9a6bd1
Viktor Nikolaev <victor.n@indicaonline.com>
2020-11-27 12:13:00 +0300

    ~

--- source/scheduler.sh
+++ source/scheduler.sh
@@ -109,21 +109,23 @@ function  aurora_mysql_cluster_switch {
                     log " new_writer = $new_writer"
                     log  "modify"
                     aws rds modify-db-instance  --db-instance-identifier $new_writer  --region $resource_region  --db-instance-class $work_writer_instance_type --apply-immediately --no-paginate
-                    log "wait "
-                    sleep 120
+                    log "wait " ;  sleep 120
                     wait_available_instance_aurora_mysql "$new_writer" "$resource_region"
                     aws rds  failover-db-cluster --db-cluster-identifier  $resource_id   --region $resource_region  --target-db-instance-identifier $new_writer --no-paginate
-                    log "wait "
-                    sleep 120
+                    log "wait " ; sleep 120
              fi
-
-
              #modify readers
-
              readers=$(get_readers_aurora_mysql_cluster "$resource_id" "$resource_region" )
              log "*** new reader = $readers"
-
-
+                for instance in $readers ; do
+                  current_reader_instance_type=$(get_instance_type_aurora_mysql "$instance" "$resource_region")
+                  if [ "$current_reader_instance_type" = "$work_reader_instance_type" ]; then
+                        echo "instance type are equal "
+                    else
+                        echo "instance not equal => change."
+                        aws rds modify-db-instance  --db-instance-identifier $instance  --region $resource_region  --db-instance-class $work_reader_instance_type --apply-immediately --no-paginate
+                  fi
+                done
            ;;
            sleep)
              log "*** sleep"
@@ -252,8 +254,7 @@ function ec2_SWITCH {
        else
            echo "instance not equal => change."
             log " $(aws ec2 stop-instances  --instance-ids $resource_id  --region $resource_region )"
-            log "sleep 60"
-            sleep 60
+            log "sleep 60" ; sleep 60
             aws ec2 modify-instance-attribute     --instance-id $resource_id      --instance-type "{\"Value\": \"$work_instance_type\"}"  --region $resource_region
             aws ec2 start-instances --instance-ids $resource_id  --region $resource_region
        fi
@@ -268,8 +269,7 @@ function ec2_SWITCH {
         else
            echo "instance not equal => change."
             log " $(aws ec2 stop-instances  --instance-ids $resource_id  --region $resource_region )"
-            log "sleep 60"
-            sleep 60
+            log "sleep 60" ; sleep 60
             aws ec2 modify-instance-attribute     --instance-id $resource_id      --instance-type "{\"Value\": \"$sleep_instance_type\"}"  --region $resource_region
             aws ec2 start-instances --instance-ids $resource_id  --region $resource_region
        fi
