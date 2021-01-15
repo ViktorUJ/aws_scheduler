ee1c8c1b65b4fb56e534d1c2318dcf7903237930
Viktor Nikolaev <victor.n@indicaonline.com>
2021-01-15 11:22:26 +0300

    ~

--- source/scheduler.sh
+++ source/scheduler.sh
@@ -657,9 +657,9 @@ function worker {
   if [[ "$id" == "all" ]] ; then
      log "id=$id , skip"
     else
-      time_stamp=$(date +%G:%m:%d_%k:%M:%S | tr -d '\n')
+      time_stamp="true: $(date +%G:%m:%d_%k:%M:%S | tr -d '\n')"
       log "id=$id set lock $time_stamp"
-      aws dynamodb update-item     --table-name scheduler_dev --key '{"id":{"S":"'$id'"}}' --attribute-updates '{"lock": {"Value": {"S": "true: '$time_stamp'"},"Action": "PUT"}}'
+      aws dynamodb update-item     --table-name scheduler_dev --key '{"id":{"S":"'$id'"}}' --attribute-updates '{"lock": {"Value": {"S": "'$time_stamp'"},"Action": "PUT"}}'
 
       echo "*****************"
       case $operational in
