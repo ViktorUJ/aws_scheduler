e9f5b8576c6466dc9aa8b96979b75ab0400cf391
Viktor Nikolaev <victor.n@indicaonline.com>
2021-02-04 16:30:00 +0300

    ~

--- install_via_terraform.tf
+++ install_via_terraform.tf
@@ -47,6 +47,29 @@ ITEM
 
 
 }
+
+
+resource "aws_dynamodb_table_item" "ec2_on_of" {
+  hash_key = "id"
+  table_name = aws_dynamodb_table.scheduler.name
+  item =  <<ITEM
+{
+  "id": {"S": "example_ec2_on_off"},
+  "operational": {"S": "false"},
+  "period_type": {"S": "work-hours"},
+  "resource_id": {"S": "i-052adc06ce62d699a"},
+  "resource_id_type": {"S": "id"},
+  "resource_region": {"S": "id"},
+  "work_hours": {"S": "0700-2100"},
+  "resource_type": {"S": "ec2"},
+  "scheduler_type": {"S": "ON_OFF"}
+    }
+ITEM
+
+
+
+}
+
 resource "aws_iam_policy" "aws_scheduler" {
   name = "aws_scheduler"
   path        = "/"
