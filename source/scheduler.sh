2fc180af1b48bb08d67ca51183b216b8d0ff9ebb
Viktor Nikolaev <victor.n@indicaonline.com>
2021-06-14 10:04:23 +0300

    ~ asg

--- source/scheduler.sh
+++ source/scheduler.sh
@@ -818,7 +818,7 @@ function asg_SWITCH {
       log "id=$id  current_need_asg_update = $current_need_asg_update"
       case $current_need_asg_update in
        true)
-         aws autoscaling update-auto-scaling-group  --auto-scaling-group-name $resource_id  --desired-capacity $sleep_desired_capacity  --maxsize $sleep_max_capacity  --min-size $sleep_min_capacity --profile $aws_profile --region $resource_region
+         aws autoscaling update-auto-scaling-group  --auto-scaling-group-name $resource_id  --desired-capacity $sleep_desired_capacity  --max-size $sleep_max_capacity  --min-size $sleep_min_capacity --profile $aws_profile --region $resource_region
          ;;
        false)
           log "id=$id  not need update asg"
@@ -830,7 +830,7 @@ function asg_SWITCH {
       log "id=$id  current_need_asg_update = $current_need_asg_update"
       case $current_need_asg_update in
        true)
-         aws autoscaling update-auto-scaling-group  --auto-scaling-group-name $resource_id  --desired-capacity $work_desired_capacity  --maxsize $work_max_capacity  --min-size $work_min_capacity --profile $aws_profile --region $resource_region
+         aws autoscaling update-auto-scaling-group  --auto-scaling-group-name $resource_id  --desired-capacity $work_desired_capacity  --max-size $work_max_capacity  --min-size $work_min_capacity --profile $aws_profile --region $resource_region
          ;;
        false)
           log "id=$id  not need update asg"
