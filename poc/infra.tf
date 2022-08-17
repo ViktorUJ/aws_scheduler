09073bf841129a6b57a66d8576401f585809c7f3
Viktor <viktar@opsfleet.com>
2022-08-17 16:54:20 +0400

    ~

--- poc/infra.tf
+++ poc/infra.tf
@@ -82,12 +82,11 @@ resource "aws_iam_policy_attachment" "aws_scheduler" {
 
 
 output "AWS_KEY" {
-  value =aws_iam_access_key.aws_scheduler.id
-   sensitive = false
+  value =nonsensitive(aws_iam_access_key.aws_scheduler.id)
 }
 
 output "AWS_SECRET" {
-  value =aws_iam_access_key.aws_scheduler.secret
+  value =nonsensitive(aws_iam_access_key.aws_scheduler.secret)
    sensitive = false
 }
 
