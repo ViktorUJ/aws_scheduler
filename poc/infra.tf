b24921b0c50cfa00497a2d7b4fafccfac5f91daa
Viktor <viktar@opsfleet.com>
2022-08-17 16:49:59 +0400

    ~

--- poc/infra.tf
+++ poc/infra.tf
@@ -31,3 +31,70 @@ resource "aws_dynamodb_table" "scheduler" {
     project = "infra"
   }
 }
+
+
+
+resource "aws_dynamodb_table_item" "all" {
+  hash_key = "id"
+  table_name = aws_dynamodb_table.scheduler.name
+  item =  <<ITEM
+{
+  "id": {"S": "all"},
+  "operational": {"S": "false"},
+  "resource_type": {"S": "all"},
+  "scheduler_type": {"S": "all"}
+    }
+ITEM
+
+
+
+}
+
+
+resource "aws_iam_policy" "aws_scheduler" {
+  name = var.resources_prefix
+  path        = "/"
+  description = "for aws_scheduler "
+  policy = file("infrastructure/aws/iam_policy.json")
+}
+
+
+resource "aws_iam_user" "aws_scheduler" {
+  name = var.resources_prefix
+  path = "/"
+}
+
+
+resource "aws_iam_access_key" "aws_scheduler" {
+  user    = aws_iam_user.aws_scheduler.name
+}
+
+
+resource "aws_iam_policy_attachment" "aws_scheduler" {
+  name = var.resources_prefix
+  policy_arn = aws_iam_policy.aws_scheduler.arn
+  groups = []
+  users = [
+    aws_iam_user.aws_scheduler.name
+  ]
+  roles = []
+}
+
+
+output "AWS_KEY" {
+  value =aws_iam_access_key.aws_scheduler.id
+}
+
+output "AWS_SECRET" {
+  value =aws_iam_access_key.aws_scheduler.secret
+}
+
+output "DYNAMODB_TABLE_NAME" {
+  value =aws_dynamodb_table.scheduler.name
+}
+
+output "DYNAMODB_REGION" {
+  value =var.region
+}
+
+
