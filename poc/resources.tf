f6e717e18baa113231233a8bb912bbb21c30baea
Viktor <viktar@opsfleet.com>
2022-08-18 12:52:06 +0400

    ~

--- poc/resources.tf
+++ poc/resources.tf
@@ -2,7 +2,7 @@ resource "random_id" "id" {
   byte_length = 10
 }
 
-resource "aws_dynamodb_table_item" "example_ec2_on_off" {
+resource "aws_dynamodb_table_item" "test-feature-env" {
   hash_key = "id"
   table_name = aws_dynamodb_table.scheduler.name
   item =  <<ITEM
