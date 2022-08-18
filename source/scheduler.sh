59e946d3e1ca56794ab01755d4c8f173cb06eba6
Viktor <viktar@opsfleet.com>
2022-08-18 14:47:55 +0400

    ~

--- source/scheduler.sh
+++ source/scheduler.sh
@@ -592,6 +592,8 @@ function feature_env_ON_OFF {
   log "rds = $rds"
   log "namespace_region = $namespace_region   namespace_eks_name = $namespace_eks_name   namespace_name = $namespace_name "
   aws eks update-kubeconfig --region  $namespace_region   --name $namespace_eks_name  --alias namespace_eks_name
+  log "==============="
+  cat  cat ~/.kube/config
   local deployments=$( kubectl get deployment -n $namespace_name --context $namespace_eks_name   -o  jsonpath='{.items[*].metadata.name}')
   logs " deployments = $deployments"
 }
