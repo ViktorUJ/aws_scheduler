f5cdcc842542ef7ff45a66c0581c9ec871bf0536
Viktor <viktar@opsfleet.com>
2022-08-18 13:41:13 +0400

    ~

---
+++ source/1.sh
@@ -0,0 +1,61 @@
+a="""
+{
+  "period_type": {
+    "S": "work-hours"
+  },
+  "resouces": {
+    "M": {
+      "NAMESPACE": {
+        "L": [
+          {
+            "M": {
+              "eks": {
+                "S": "doordawn"
+              },
+              "name": {
+                "S": "featute-sjfjksjdkfsdjk"
+              },
+              "region": {
+                "S": "eu-west-1"
+              }
+            }
+          }
+        ]
+      },
+      "RDS": {
+        "L": [
+          {
+            "M": {
+              "name": {
+                "S": "by-data-base"
+              },
+              "region": {
+                "S": "eu-west-1"
+              }
+            }
+          }
+        ]
+      }
+    }
+  },
+  "resource_type": {
+    "S": "feature_env"
+  },
+  "lock": {
+    "S": ""
+  },
+  "scheduler_type": {
+    "S": "ON_OFF"
+  },
+  "id": {
+    "S": "Ava0Qy2mXA6AtA"
+  },
+  "operational": {
+    "S": "true"
+  },
+  "work_hours": {
+    "S": "0700-2100"
+  }
+}
+"""
+echo $A
